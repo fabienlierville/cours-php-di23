0ce59699edfb267475323f1d0bd6b33d77c9320f
Fabien LIERVILLE <flierville@cesi.fr>
2024-11-15 08:53:46 +0100

    fonction de check role

---
+++ inc/security.php
@@ -0,0 +1,22 @@
+<?php
+
+function haveGoodRole(array $rolesCompatibles) :bool {
+    // Par exemple pour la page ajout Article il faudra être soit Admin soit Redacteur
+    //Donc dans $rolesCompatibles on enverra ["Admin","Redacteur"]
+
+    if(!isset($_SESSION["Login"])){
+        return false;
+    }
+    //Comparaison role par role
+    // On a un tableau de role compatible
+    // Dans la session on un tableau de role pour l'utilisateur
+    $roleFound = false;
+    $rolesSession = json_decode($_SESSION["Login"]["Roles"]);
+    foreach($rolesSession as $role){
+        if(in_array($role, $rolesCompatibles)){
+            $roleFound = true;
+            break;
+        }
+    }
+    return $roleFound;
+}
