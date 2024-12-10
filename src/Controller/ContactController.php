b47f9a64f6b26c7ba75c8608729e8ce6884c4891
fabien <flierville@cesi.fr>
2024-12-10 15:56:32 +0100

    Mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,6 +1,7 @@
 <?php
 namespace src\Controller;
 
+use src\Service\MailService;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mime\Email;
@@ -12,16 +13,16 @@ class ContactController extends AbstractController {
     }
 
     public function send(){
-        // Instancier / configurer Symfony Mailer
-        $transport = Transport::fromDsn("smtp://3dd84281bc8679:8a9180301c670a@sandbox.smtp.mailtrap.io:2525");
-        $mailer = new Mailer($transport);
-        // Concevoir & envoyer le message
-        $email = (new Email())
-            ->from($_POST["mail"])
-            ->to("admin@blogcesi.fr")
-            ->subject("Nouveau message de contact")
-            ->html("<h1>Nouveau message de contact</h1> {$_POST["message"]}");
-        $mailer->send($email);
+        $mail = new MailService();
+        $mail->send(
+            from: $_POST["mail"]
+            ,to: "admin@blogcesi.fr"
+            ,subjet: "Nouveau message de contact"
+            ,html: $this->twig->render('Mailing/contact.html.twig', [
+                    "nom" => $_POST["nom"],
+                    "message" => $_POST["message"],
+                ])
+        );
         header("location:/Contact/form");
     }
 }
