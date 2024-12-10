cca56997f9ae257dbc48b3120c21d997b9096328
fabien <flierville@cesi.fr>
2024-12-10 11:31:01 +0100

    API Interne search

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,25 @@
 //alert("JS du fichier script.js général");
+
+$("#Search").autocomplete({
+    source: function (request, response){
+        $.ajax({
+            url: "http://www.cesi.local/ApiArticle/search",
+            dataType: "json",
+            data: JSON.stringify({"keyword": request.term}),
+            type: "POST",
+            beforeSend: function () {
+                console.log("Waiting ....");
+            },
+            success: function (data){
+                console.log(data);
+                response([]);
+            },
+            error: function (){
+                console.error("Erreur appel API");
+                response([]);
+            }
+        })
+    }
+
+
+});
